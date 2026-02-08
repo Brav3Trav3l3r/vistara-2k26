2df1075a7b39609b49f62785fecfde423c27ad14
Brav3Trav3l3r <yashgajbhiye187@gmail.com>
2026-02-08 14:01:17 +0530

    add page scroll

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,72 @@
+import MegaScroll from "react-mega-scroll";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { useState } from "react";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Vistara 2k26" },
+    { name: "description", content: "Welcome to JSS College" },
   ];
 }
 
+const styles: React.CSSProperties = {};
+
 export default function Home() {
-  return <Welcome />;
+  const [active, setActive] = useState(0);
+  return (
+    <>
+      <nav className="fixed top-4 inset-x-4 bg-white shadow-xl rounded-lg z-50 p-4 flex justify-between items-center">
+        <div className="text-xl font-bold text-gray-800">Vistara 2k26</div>
+        <ul className="flex space-x-4">
+          <li>
+            <a href="#" className="text-gray-600 hover:text-indigo-600">
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#" className="text-gray-600 hover:text-indigo-600">
+              About
+            </a>
+          </li>
+          <li>
+            <a href="#" className="text-gray-600 hover:text-indigo-600">
+              Contact
+            </a>
+          </li>
+        </ul>
+      </nav>
+
+      <MegaScroll onChange={setActive}>
+        <div
+          style={{
+            ...styles,
+            background: "#111",
+            backgroundImage:
+              'linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0) 50%), url("/images/clg-img1.jpg")', // Added linear-gradien
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+          className="flex"
+        >
+          <header className="flex flex-col justify-end p-10">
+            <div className="text-2xl my-4 font-bold text-gray-800 dark:text-white">
+              Experience Global Connection and Unity
+            </div>
+            <p className="max-w-prose">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores,
+              corporis excepturi? Praesentium eligendi nobis quae expedita
+              molestias
+            </p>
+            <button className="rounded-sm border border-indigo-600 bg-indigo-600 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-indigo-600">
+              Register
+            </button>
+          </header>
+        </div>
+        <div style={{ ...styles, background: "#222" }}>Smooth Scrolling</div>
+        <div style={{ ...styles, background: "#333" }}>Keyboard Support</div>
+        <div style={{ ...styles, background: "#222" }}>State Listeners</div>
+        <div style={{ ...styles, background: "#111" }}>Free Forever</div>
+      </MegaScroll>
+    </>
+  );
 }
