dc448c222cc70f4cf04e878a03d827c110bc361b
Brav3Trav3l3r <yashgajbhiye187@gmail.com>
2026-02-13 23:39:45 +0530

    remove navbar

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,7 +1,6 @@
 import MegaScroll from "react-mega-scroll";
 import type { Route } from "./+types/home";
 import { useState } from "react";
-import Navbar from "../components/navbar";
 import About from "~/components/about";
 import Events from "~/components/events";
 import Register from "~/components/register";
@@ -10,7 +9,7 @@ import Hero from "~/components/hero";
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "Vistara 2k26" },
-    { name: "description", content: "Welcome to JSS College" },
+    { name: "description", content: "Welcome to Vistara 2k26" },
   ];
 }
 
@@ -20,14 +19,11 @@ export default function Home() {
   const [active, setActive] = useState(0);
 
   return (
-    <>
-      {/* TODO BE REMOVED <Navbar /> */}
-      <MegaScroll onChange={setActive}>
-        <Hero styles={{ ...styles }} />
-        <About styles={{ ...styles }} />
-        <Events styles={{ ...styles }} />
-        <Register styles={{ ...styles }} />
-      </MegaScroll>
-    </>
+    <MegaScroll onChange={setActive}>
+      <Hero styles={{ ...styles }} />
+      <About styles={{ ...styles }} />
+      <Events styles={{ ...styles }} />
+      <Register styles={{ ...styles }} />
+    </MegaScroll>
   );
 }
